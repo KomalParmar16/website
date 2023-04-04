7d71d0cfec7edeb28f8aecddd00dbac032f48c93
KomalParmar16 <komalparmar.it@gmail.com>
2023-04-04 14:05:39 +0530

    delete some file

--- deploy.js
+++
@@ -1,7 +0,0 @@
-// Add the deployment script over here!
-const deployPlugin = () => {
-    // eslint-disable-next-line no-console
-    console.log('Deployment Successful!');
-  };
-
-  deployPlugin();
